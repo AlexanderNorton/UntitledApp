29fef3656271f66fe1dcf560de42512cdaf9a08c
Alexander Norton <alex.norton15@gmail.com>
2022-08-20 18:07:20 +0100

    Modified the profile list HTML code to display a list of user profiles created in the database

--- profile_app/views.py
+++ profile_app/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Profile
 
 def profile_list(request):
-    return render(request, 'profile_list.html', {})
+    profiles = Profile.objects.filter(updated_date__lte=timezone.now()).order_by('updated_date')
+    return render(request, 'profile_list.html', {'profiles': profiles})
