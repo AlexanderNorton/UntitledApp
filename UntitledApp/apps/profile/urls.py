042dfe13a5c6c5785327f0c3baef09def3446acc
Alexander Norton <alex.norton15@gmail.com>
2022-09-17 20:17:07 +0100

    Added an API endpoint for obtaining profiles list

--- UntitledApp/apps/profile/urls.py
+++ UntitledApp/apps/profile/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .api import ProfileList
 
 urlpatterns = [
+    path('', ProfileList.as_view()),
     path('<str:owner>/', views.profile, name='profile'),
 ]
